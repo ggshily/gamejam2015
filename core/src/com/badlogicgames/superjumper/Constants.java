bb2c2e6824921df5e82ab964e1782c6788b58374
haogang <haogang.chen@nebuliumgames.com>
2015-01-24 18:55:36 +0800

    add super jamper

---
+++ core/src/com/badlogicgames/superjumper/Constants.java
@@ -0,0 +1,10 @@
+package com.badlogicgames.superjumper;
+
+/**
+ * Created by gg_shily on 1/24/15.
+ */
+public class Constants {
+
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+}
